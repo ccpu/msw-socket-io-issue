c207463598a185215ba117899a2b6d69cc897930
cyrus <mdoaie@yahoo.co.uk>
2025-03-09 19:36:47 +0000

    try suggestions

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import { toSocketIo } from "@mswjs/socket.io-binding";
 import { ws } from "msw";
 import { setupWorker } from "msw/browser";
 
-const chat = ws.link("ws://localhost:3000/socket.io/");
+const chat = ws.link("ws://localhost:3000");
 
 export const handlers = [
   chat.addEventListener("connection", (connection) => {
     const io = toSocketIo(connection);
+
+    console.log("Connected");
 
     io.client.on("connect", () => {
       io.client.emit("message", "Connected to the server!");
@@ -17,17 +19,22 @@ export const handlers = [
 
 const worker = setupWorker(...handlers);
 
-worker.start().then(() => {
-  // const ws = new WebSocket("ws://localhost:3000/socket.io/?EIO=4&transport=websocket");
+worker
+  .start({
+    onUnhandledRequest(request) {
+      console.log(`Oops, unhandled ${request.url}`);
+    },
+  })
+  .then(() => {
+    // const ws = new WebSocket("ws://localhost:3000");
 
-  const socket = io("ws://localhost:3000", {
-    retries: 1,
-    transports: ["websocket"],
-    port: 3000,
+    const socket = io("ws://localhost:3000", {
+      transports: ["websocket"],
+      path: "/",
+    });
+
+    socket.connect();
   });
-
-  socket.connect();
-});
 
 export default function App() {
   return <div></div>;
